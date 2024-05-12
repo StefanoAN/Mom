b3a472543aa1caba56a55d1404e695387a6923eb
StefanoAN <168984369+StefanoAN@users.noreply.github.com>
2024-05-12 00:26:22 -0500

    script.js
    mom

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("sobre").classList.add("oculto");
+        document.getElementById("carta").classList.remove("oculto");
+    }, 3000);
+});
